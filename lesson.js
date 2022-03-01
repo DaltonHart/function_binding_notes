f5faf4ea8609b3144910387a03a892ce648d352c
DaltonHart <daltonvaren@gmail.com>
2022-03-01 08:23:48 -0800

    init commit

---
+++ lesson.js
@@ -0,0 +1,11 @@
+console.log("%cFunction Binding ⚙️", "padding: 10px; background: rebeccapurple; border-bottom: 3px solid orchid; font-weight: bold;");
+
+/* === Function Binding === */
+
+/*
+  == GAMEPLAN == 
+  1. Review `this` 
+  2. The case for binding 
+  3. Using a bind function
+*/
+
