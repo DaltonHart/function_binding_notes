5012e92335e1fc868374fda02f2933b8e2fde908
DaltonHart <daltonvaren@gmail.com>
2022-03-01 09:26:00 -0800

    updates gameplan and log color

--- lesson.js
+++ lesson.js
@@ -5,7 +5,7 @@
  * Want to learn how to style logs? Check out the following link
  * @url https://css-tricks.com/a-guide-to-console-commands/#aa-styling-the-output
  */
-console.log("%cFunction Binding ⚙️", "padding: 10px; background: rebeccapurple; border-bottom: 3px solid orchid; font-weight: bold;");
+console.log("%cFunction Binding ⚙️", "padding: 10px; background: rebeccapurple; border-bottom: 3px solid orchid; font-weight: bold; color: whitesmoke;");
 
 /* === Function Binding === */
 
@@ -13,6 +13,8 @@ console.log("%cFunction Binding ⚙️", "padding: 10px; background: rebeccapurp
   == GAMEPLAN == 
   1. Review `this` 
   2. The case for binding 
-  3. Using a bind function
+  3. Using a bind function* 
+
+  *example purpose only js has a built in .bind
 */
 
